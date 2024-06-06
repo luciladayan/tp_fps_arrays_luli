ec044c1ff391c4ed68c3018a61c1242da4a18f3f
luciladayan <163851662+luciladayan@users.noreply.github.com>
2024-06-06 12:16:32 -0300

    tp a terminar

---
+++ Assets/personagescript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personagescript : MonoBehaviour
+{
+    public int nivelHambre;
+    void TomarAlimento(int valorAlimentario)
+    {
+
+    }
+        
+    private void OnTriggerEnter(Collider other)
+        {
+
+
+            AlimentoScript alimento;
+            alimento = other.GetComponent<AlimentoScript>();
+            if (alimento)
+
+            {
+
+                TomarAlimento(alimento.valorAlimentario);
+                Destroy(other.gameObject);
+
+
+            }
+        }
+
+
+    }
